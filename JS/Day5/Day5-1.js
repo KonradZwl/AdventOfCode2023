0d892f18b4aacc054d8600acb0b290a413ea54ee
Konrad Zwolinski <konradz@delta-n.nl>
2023-12-05 15:17:16 +0100

    Day 5 part 1

---
+++ JS/Day5/Day5-1.js
@@ -0,0 +1,35 @@
+const {input} = require('./Day5Input');
+
+let locations = [];
+
+
+//Get seed numbers
+const seedNumbers = input[0].split(":")[1].trim().split(" ").map(x => parseInt(x))
+//Create each map
+
+for (let x = 0; x < seedNumbers.length; x++){
+    let destinationNumber = seedNumbers[x]
+
+    for (let i = 2; i < input.length; i++) {
+        //Map section
+        if (input[i].includes("map")){
+            console.log(input[i])
+            let j = i + 1;
+            while (input[j] !== "\r" && j !== input.length){
+                let tempDestination = parseInt(input[j].split(" ")[0]);
+                let tempSource = parseInt(input[j].split(" ")[1]);
+                let tempLength = parseInt(input[j].split(" ")[2]);
+                
+                if (destinationNumber <= tempSource + tempLength && destinationNumber >= tempSource){ 
+                    let temp = tempDestination - tempSource;
+                    destinationNumber =  destinationNumber + temp;
+                    break;
+                }
+                j++;
+            }
+            i = j;
+        }
+    }
+    locations.push(destinationNumber);
+}
+console.log(Math.min(...locations))
