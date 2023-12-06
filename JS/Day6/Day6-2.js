0bc0aaaf26f3c9ac47dbeb25ea3aee687e50960d
Konrad Zwolinski <konradz@delta-n.nl>
2023-12-06 12:53:23 +0100

    Optimized from 10.857s to 5.489s

--- JS/Day6/Day6-2.js
+++ JS/Day6/Day6-2.js
@@ -1,11 +1,28 @@
+console.time("test_timer")
 const {input} = require('./Day6Input');
 
 let time = input[0].split(":")[1].trim().replace(/\s+/g, '');
 let distance = input[1].split(":")[1].trim().replace(/\s+/g, '');
 
 let fasterThanRecord = 0;
-for (let i = 0; i <= parseInt(time); i++){
-        if ((parseInt(time) - i) * i > parseInt(distance))
-            fasterThanRecord++;
+let numberIsEven = false;
+if (Math.round(parseInt(time) / 2) % 2 === 0){
+    numberIsEven = true;
 }
-console.log(fasterThanRecord);
+
+let i = Math.round(parseInt(time) / 2);
+while ((parseInt(time) - i) * i !== 0){
+    if ((parseInt(time) - i) * i > parseInt(distance)){
+        fasterThanRecord++;
+    }
+    i++;
+}
+
+let total;
+
+total = fasterThanRecord * 2;
+if (!numberIsEven){
+    total -= 1;
+}
+console.log(total)
+console.timeEnd("test_timer")
